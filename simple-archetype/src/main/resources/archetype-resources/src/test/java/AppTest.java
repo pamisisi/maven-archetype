89285fcac12fa7584a0bd84074431c0803a8d11f
Pamisisi <pamisisi@live.com>
2013-11-13 22:09:42 +0800

    first commit

---
+++ simple-archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,15 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class AppTest  {
+
+	@Test
+    public void testApp() {
+        assertTrue( true );
+    }
+}
